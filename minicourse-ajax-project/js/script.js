9a00cf3502fea503a682d1e9cb56b2469dea68a8
pepperpapa <zhongxin2506@outlook.com>
2016-03-15 22:30:42 +0800

    finish NY times api quiz

--- minicourse-ajax-project/js/script.js
+++ minicourse-ajax-project/js/script.js
@@ -11,17 +11,36 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
+
+    $greeting.text('So, you want to live at ' + address + '?');
+
+
     // load streetview
-    $body.append("<img class='bgimg'" +
-    "src = 'https://maps.googleapis.com/maps/api/streetview?" +
-    "size=600x300&location=" + $('#street').val() + "," + $('#city').val() + "&" +
-    "heading=151.78&pitch=0&" +
-    "key=AIzaSyDECcFSTGlCwrIY6Oopv87JozbQDkko_e0' alt='background image'>");
-    // YOUR CODE GOES HERE!
-    console.log($('#street').val(), $('#city').val());
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+
+    // load nytimes
+    var ntyURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?" +
+        "q=" + cityStr + "&page=2&sort=oldest&" +
+        "api-key=6c8e57991597a9eaefac86510963b66b:13:74704165";
+    var $articleListElm = $('.article-list');
+    $.getJSON(ntyURL,  function (data) {
+      $.each(data.response.docs, function(key, val) {
+        console.log(val);
+        $nytHeaderElem.text("New York Times Articles about " + cityStr);
+        $articleListElm.append("<li id='" + key + "'>" +
+                "<a href='" + val.web_url + "'>" + val.headline.main + "</a>" +
+                "<p>" + val.snippet + "</p>");
+      });
+    }).error(function () {
+      $nytHeaderElem.text("New York Times Articles can't be loaded!");
+    });
+
     return false;
 }
 
 $('#form-container').submit(loadData);
-
-// loadData();
