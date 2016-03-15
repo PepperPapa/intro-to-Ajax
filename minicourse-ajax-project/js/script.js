3e5e70154aef26cf116e6e971adf1679f6f23a6c
pepperpapa <zhongxin2506@outlook.com>
2016-03-15 23:35:17 +0800

    finish intro to ajax course

--- minicourse-ajax-project/js/script.js
+++ minicourse-ajax-project/js/script.js
@@ -1,6 +1,9 @@
 
 function loadData() {
 
+    /*
+    The $ that shows up in variable names, like $body for example, is just a character like any other. In this case, it refers to the fact that the variable referenced by $body is a jQuery collection, not a DOM node.
+    */
     var $body = $('body');
     var $wikiElem = $('#wikipedia-links');
     var $nytHeaderElem = $('#nytimes-header');
@@ -24,22 +27,48 @@ function loadData() {
 
 
     // load nytimes
-    var ntyURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?" +
-        "q=" + cityStr + "&page=2&sort=oldest&" +
-        "api-key=6c8e57991597a9eaefac86510963b66b:13:74704165";
-    var $articleListElm = $('.article-list');
-    $.getJSON(ntyURL,  function (data) {
-      $.each(data.response.docs, function(key, val) {
-        console.log(val);
-        $nytHeaderElem.text("New York Times Articles about " + cityStr);
-        $articleListElm.append("<li id='" + key + "'>" +
-                "<a href='" + val.web_url + "'>" + val.headline.main + "</a>" +
-                "<p>" + val.snippet + "</p>");
-      });
-    }).error(function () {
-      $nytHeaderElem.text("New York Times Articles can't be loaded!");
+    // obviously, replace all the "X"s with your own API key
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=6c8e57991597a9eaefac86510963b66b:13:74704165';
+    $.getJSON(nytimesUrl, function(data){
+
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">'+
+                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
+                '<p>' + article.snippet + '</p>'+
+            '</li>');
+        }
+
+    }).error(function(e){
+        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
     });
 
+
+    // load wikipedia data
+    var wikiURL = 'https://en.wikipedia.org/w/api.php?' +
+      'action=opensearch&search=' + cityStr +
+      '&format=json&callback=wikiCallback';
+    var  wikiRequestTimeout = setTimeout(function() {
+      $wikiElem.text("wikipedia have no response.");
+    }, 8000);
+
+    $.ajax({
+      url: wikiURL,
+      dataType: "jsonp",
+      // jsonp: "callback",
+      success: function (response) {
+        console.log(response);
+        for (var i = 0; i < response[1].length; i++) {
+          articleStr = response[1][i];
+          $wikiElem.append('<li><a href="' + response[3][i] + '">' +
+                articleStr + '</a></li>');
+        }
+        clearTimeout(wikiRequestTimeout);
+      }
+    });
     return false;
 }
 
